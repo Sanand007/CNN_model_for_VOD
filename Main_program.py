86216d24a697c8b74fc0de672624c5c6d221feb3
Sanand007 <smit.anand022@gmail.com>
2017-04-08 12:51:11 -0400

    ready to make .bin

--- Main_program.py
+++ Main_program.py
@@ -1,13 +1,56 @@
-import urllib
+from urllib.request import urlretrieve
 import csv
+import os
+import subprocess
 
 from pandas import read_csv
 
+counter = 0
+
+os.makedirs('classes')
 filepath = 'english.csv'
 names = ['ANP', 'image_url']
 dataframe = read_csv(filepath, names=names)
 array = dataframe.values
 X = array[1:,:]
 print(len(X))
-
-#urllib.urlretrieve("http://farm1.staticflickr.com/215/517510757_9ced246a66.jpg", "local-filename.jpg")
+image_num = 0
+path = ""
+filepath = ""
+with open('batches.meta.txt', 'w') as meta_file:
+	string = ""
+	for i in range(0, len(X)):
+		if (i == 0):
+			meta_file.write(X[0][0] + '\n')
+			path = 'classes/' + str(counter)
+			os.makedirs(path)
+			filepath = path + '/' + str(image_num) + '.jpg'
+			image_num += 1
+			urlretrieve(str(X[0][1]), filepath)
+			counter += 1
+			string = str(X[0][0])
+		elif (string != str(X[i][0])):
+			meta_file.write(str(X[i][0]) + '\n')
+			path = 'classes/' + str(counter)
+			os.makedirs(path)
+			image_num = 0
+			filepath = path + '/' + str(image_num) + '.jpg'
+			image_num += 1
+			urlretrieve(str(X[i][1]), filepath)
+			counter+=1
+			string = str(X[i][0])
+		elif (image_num < 6):
+			filepath = path + '/' + str(image_num) + '.jpg'
+			image_num += 1
+			urlretrieve(str(X[i][1]), filepath)
+		if (counter == 3):
+			print(i)
+			break
+meta_file.close()
+print("Classes names written in batches.meta.txt file")
+print("Images downloaded in separate folders under classes")
+print("Converting images to 32*32 format")
+subprocess.call(['./resize-script.sh'])
+print("Image conversion to 32*32 done")
+print("Calling python code to convert image to .bin format")
+os.system("python convert-images-to-cifar-format.py")
--- convert-images-to-cifar-format.py
+++ convert-images-to-cifar-format.py
@@ -1,4 +1,5 @@
 #python script for converting 32x32 pngs to format
+
 from PIL import Image
 import os
 from array import *
@@ -7,45 +8,19 @@ data = array('B')
 
 for dirname, dirnames, filenames in os.walk('./classes'):
     for filename in filenames:
-	if filename.endswith('.png'):
+        if (filename.endswith('.jpg')):
+            im = Image.open(os.path.join(dirname, filename))
+            pix = im.load()
 
-	    ################
-	    #grab the image#
-	    ################
+            class_name = int(os.path.join(dirname).split('/')[-1])
 
-	    im = Image.open(os.path.join(dirname, filename))
-	    pix = im.load()
-	    #print(os.path.join(dirname, filename))
+            data.append(class_name)
 
-	    #store the class name from look at path
-	    class_name = int(os.path.join(dirname).split('/')[-1])
-	    #print class_name
+            for color in range(0,3):
+                for x in range(0,32):
+                    for y in range(0,32):
+                        data.append(pix[x,y][color])
 
-	    ###########################
-	    #get image into byte array#
-	    ###########################
-
-	    # create array of bytes to hold stuff
-
-	    #first append the class_name byte
-	    data.append(class_name)
-
-	    #then write the rows
-	    #Extract RGB from pixels and append
-	    #note: first we get red channel, then green then blue
-	    #note: no delimeters, just append for all images in the set
-	    for color in range(0,3):
-		for x in range(0,32):
-		    for y in range(0,32):
-			data.append(pix[x,y][color])
-
-
-############################################
-#write all to binary, all set for cifar10!!#
-############################################
-
-output_file = open('cifar10-ready.bin', 'wb')
+output_file = open('cnn-ready.bin', 'wb')
 data.tofile(output_file)
 output_file.close()
-
-
